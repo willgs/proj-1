b61c001094ac8ccf8d2b68bec390eb4df4234f03
willgs <31709696+willgs@users.noreply.github.com>
2017-09-28 12:40:43 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,35 +1,40 @@
-#include <time.h> 
+#include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
-pragma pack(1)
+#pragma pack(1);
+
+typedef unsigned int DWORD;
+typedef short WORD;
+
 
 struct BMPhead{
     WORD format;
     DWORD size;
-    WORD resVal1;
+    DWORD resVal1;
     WORD resVal2;
     DWORD Offset;
 };
 
 struct DIBhead{
     DWORD headSize;
-    LONG width;
-    LONG height;
+    long width;
+    long height;
     WORD planes;
     WORD bCount;
     DWORD comp;
     DWORD imageSize;
-    LONG horRes;
+    long horRes;
 };
 
-int main(char[] args){
+int main(char args[]){
 
     struct BMPhead bmphead;
     struct DIBhead dibhead;
-    FILE *file;
+    FILE *img;
 
-    file = fopen(args, 'r');
+    img = fopen(args, 'r');
 
-    fread(&bmphead, sizeof(bmphead), 1, file);
+    fread(&bmphead, sizeof(bmphead), 1, img);
 
     if(bmphead.format != 19778)
     {
@@ -37,37 +42,26 @@ int main(char[] args){
         return 0;
     }
 
-    fread(&dibhead, sizeof(dibhead), 1, file);
+    fread(&dibhead, sizeof(dibhead), 1, img);
 
     if(dibhead.headSize != 40){
-        printf("Unsupported file type, you may try again.")
+        printf("Unsupported file type, you may try again.");
         return 0;
     }
     
      if(dibhead.bCount != 24){
-        printf("Unsupported file type, you may try again. ")
+        printf("Unsupported file type, you may try again. ");
         return 0;
     }
 
-    fseek(file, bmphead.Offset, SEEK_SET);
+    fseek(img, bmphead.Offset, SEEK_SET);
 
-    unsigned char *bmpImage = (insigned
-char*)malloc(dibhead->imageSize);
+    unsigned char *bmpImage = (unsigned char*)malloc(dibhead.imageSize);
 
-    fread(bmpImage, dibhead->imageSize, file);
-
-        printf("Unsupported file type, you may try again. ")
-        return 0;
-    }
-
-    fseek(file, bmphead.Offset, SEEK_SET);
+    fread(bmpImage, sizeOf(unsigned int), dibhead.imageSize, img);
     
-    unsigned char *bmpImage = (insigned char*)malloc(dibhead->imageSize);
-
-    fread(bmpImage, dibhead->imageSize, file);
-
     bmpImage = ~bmpImage;
 
-    fseek(file, bmhhead.Offset, SEEK_SET);
-    fwrite(bmpImage, dibhead->imageSize, file);
+    fseek(img, bmhhead.Offset, SEEK_SET);
+    fwrite(bmpImage, dibhead->imageSize, img);
 }
